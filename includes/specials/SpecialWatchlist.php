bluelovers/mediawiki-trunk-phase3

commit 0ab6432041f6c5ec6eaa74d339464a2041e7dc75

* replace use of deprecated makeKnownLinkObj() by linkKnown() in core special pages * use array type parameter instead of string to escapeLocalUrl(), getFullURL() and getFullUrl() for readability * some code readability and indentation improvements
Linking this to r51559 for CodeReview as there is some discussion there, and these changes are very similar.

git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@51572 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/specials/SpecialWatchlist.php
@@ -21,8 +21,12 @@ function wfSpecialWatchlist( $par ) {
 	# Anons don't get a watchlist
 	if( $wgUser->isAnon() ) {
 		$wgOut->setPageTitle( wfMsg( 'watchnologin' ) );
-		$llink = $skin->makeKnownLinkObj( SpecialPage::getTitleFor( 'Userlogin' ), 
-			wfMsgHtml( 'loginreqlink' ), 'returnto=' . $specialTitle->getPrefixedUrl() );
+		$llink = $skin->linkKnown(
+			SpecialPage::getTitleFor( 'Userlogin' ), 
+			wfMsgHtml( 'loginreqlink' ),
+			array(),
+			array( 'returnto' => $specialTitle->getPrefixedUrl() )
+		);
 		$wgOut->addHTML( wfMsgWikiHtml( 'watchlistanontext', $llink ) );
 		return;
 	}
@@ -256,34 +260,100 @@ function wfSpecialWatchlist( $par ) {
 	$hideLinktext = wfMsgHtml( 'hide' );
 	# Hide/show minor edits
 	$label = $hideMinor ? $showLinktext : $hideLinktext;
-	$linkBits = wfArrayToCGI( array( 'hideMinor' => 1 - (int)$hideMinor ), $nondefaults );
-	$links[] = wfMsgHtml( 'rcshowhideminor', $skin->makeKnownLinkObj( $thisTitle, $label, $linkBits ) );
+	$linkBits = array_merge(
+		array( 'hideMinor' => 1 - (int)$hideMinor ),
+		$nondefaults
+	);
+	$links[] = wfMsgHtml(
+		'rcshowhideminor',
+		$skin->linkKnown(
+			$thisTitle,
+			$label,
+			array(),
+			$linkBits
+		)
+	);
 
 	# Hide/show bot edits
 	$label = $hideBots ? $showLinktext : $hideLinktext;
-	$linkBits = wfArrayToCGI( array( 'hideBots' => 1 - (int)$hideBots ), $nondefaults );
-	$links[] = wfMsgHtml( 'rcshowhidebots', $skin->makeKnownLinkObj( $thisTitle, $label, $linkBits ) );
+	$linkBits = array_merge(
+		array( 'hideBots' => 1 - (int)$hideBots ),
+		$nondefaults
+	);
+	$links[] = wfMsgHtml(
+		'rcshowhidebots',
+		$skin->linkKnown(
+			$thisTitle,
+			$label,
+			array(),
+			$linkBits
+		)
+	);
 
 	# Hide/show anonymous edits
 	$label = $hideAnons ? $showLinktext : $hideLinktext;
-	$linkBits = wfArrayToCGI( array( 'hideAnons' => 1 - (int)$hideAnons ), $nondefaults );
-	$links[] = wfMsgHtml( 'rcshowhideanons', $skin->makeKnownLinkObj( $thisTitle, $label, $linkBits ) );
+	$linkBits = array_merge(
+		array( 'hideAnons' => 1 - (int)$hideAnons ),
+		$nondefaults
+	);
+	$links[] = wfMsgHtml(
+		'rcshowhideanons',
+		$skin->linkKnown(
+			$thisTitle,
+			$label,
+			array(),
+			$linkBits
+		)
+	);
 
 	# Hide/show logged in edits
 	$label = $hideLiu ? $showLinktext : $hideLinktext;
-	$linkBits = wfArrayToCGI( array( 'hideLiu' => 1 - (int)$hideLiu ), $nondefaults );
-	$links[] = wfMsgHtml( 'rcshowhideliu', $skin->makeKnownLinkObj( $thisTitle, $label, $linkBits ) );
+	$linkBits = array_merge(
+		array( 'hideLiu' => 1 - (int)$hideLiu ),
+		$nondefaults
+	);
+	$links[] = wfMsgHtml(
+		'rcshowhideliu',
+		$skin->linkKnown(
+			$thisTitle,
+			$label,
+			array(),
+			$linkBits
+		)
+	);
 
 	# Hide/show own edits
 	$label = $hideOwn ? $showLinktext : $hideLinktext;
-	$linkBits = wfArrayToCGI( array( 'hideOwn' => 1 - (int)$hideOwn ), $nondefaults );
-	$links[] = wfMsgHtml( 'rcshowhidemine', $skin->makeKnownLinkObj( $thisTitle, $label, $linkBits ) );
+	$linkBits = array_merge(
+		array( 'hideOwn' => 1 - (int)$hideOwn ),
+		$nondefaults
+	);
+	$links[] = wfMsgHtml(
+		'rcshowhidemine',
+		$skin->linkKnown(
+			$thisTitle,
+			$label,
+			array(),
+			$linkBits
+		)
+	);
 
 	# Hide/show patrolled edits
 	if( $wgUser->useRCPatrol() ) {
 		$label = $hidePatrolled ? $showLinktext : $hideLinktext;
-		$linkBits = wfArrayToCGI( array( 'hidePatrolled' => 1 - (int)$hidePatrolled ), $nondefaults );
-		$links[] = wfMsgHtml( 'rcshowhidepatr', $skin->makeKnownLinkObj( $thisTitle, $label, $linkBits ) );
+		$linkBits = array_merge(
+			array( 'hidePatrolled' => 1 - (int)$hidePatrolled ),
+			$nondefaults
+		);
+		$links[] = wfMsgHtml(
+			'rcshowhidepatr',
+			$skin->linkKnown(
+				$thisTitle,
+				$label,
+				array(),
+				$linkBits
+			)
+		);
 	}
 
 	# Namespace filter and put the whole form together.

includes/specials/SpecialAllmessages.php
@@ -52,8 +52,8 @@ function wfSpecialAllmessages() {
 		$navText .= wfAllMessagesMakePhp( $messages );
 		$wgOut->addHTML( $wgLang->pipeList( array(
 			'PHP',
-			'<a href="' . $title->escapeLocalUrl( 'ot=html' ) . '">HTML</a>',
-			'<a href="' . $title->escapeLocalUrl( 'ot=xml' ) . '">XML</a>' .
+			'<a href="' . $title->escapeLocalUrl( array( 'ot' => 'html' ) ) . '">HTML</a>',
+			'<a href="' . $title->escapeLocalUrl( array( 'ot' => 'xml' ) ) . '">XML</a>' .
 			'<pre>' . htmlspecialchars( $navText ) . '</pre>'
 		) ) );
 	} else if ( $ot == 'xml' ) {
@@ -62,9 +62,9 @@ function wfSpecialAllmessages() {
 		echo wfAllMessagesMakeXml( $messages );
 	} else {
 		$wgOut->addHTML( $wgLang->pipeList( array(
-			'<a href="' . $title->escapeLocalUrl( 'ot=php' ) . '">PHP</a>',
+			'<a href="' . $title->escapeLocalUrl( array( 'ot' => 'php' ) ) . '">PHP</a>',
 			'HTML',
-			'<a href="' . $title->escapeLocalUrl( 'ot=xml' ) . '">XML</a>'
+			'<a href="' . $title->escapeLocalUrl( array( 'ot' => 'xml' ) ) . '">XML</a>'
 		) ) );
 		$wgOut->addWikiText( $navText );
 		$wgOut->addHTML( wfAllMessagesMakeHTMLText( $messages ) );
@@ -188,14 +188,17 @@ function wfAllMessagesMakeHTMLText( &$messages ) {
 		$mw = htmlspecialchars( $m['msg'] );
 
 		if( array_key_exists( $title, $pageExists[NS_MEDIAWIKI] ) ) {
-			$pageLink = $sk->makeKnownLinkObj( $titleObj, "<span id=\"sp-allmessages-i-$i\">" . 
-				htmlspecialchars( $key ) . '</span>' );
+			// FIXME: the span should be taken care of in $customAttribs, shouldn't it?
+			$pageLink = $sk->linkKnown(
+				$titleObj,
+				"<span id=\"sp-allmessages-i-$i\">" . htmlspecialchars( $key ) . '</span>'
+			);
 		} else {
 			$pageLink = $sk->makeBrokenLinkObj( $titleObj, "<span id=\"sp-allmessages-i-$i\">" . 
 				htmlspecialchars( $key ) . '</span>' );
 		}
 		if( array_key_exists( $title, $pageExists[NS_MEDIAWIKI_TALK] ) ) {
-			$talkLink = $sk->makeKnownLinkObj( $talkPage, htmlspecialchars( $talk ) );
+			$talkLink = $sk->linkKnown( $talkPage, htmlspecialchars( $talk ) );
 		} else {
 			$talkLink = $sk->makeBrokenLinkObj( $talkPage, htmlspecialchars( $talk ) );
 		}

includes/specials/SpecialAllpages.php
@@ -70,45 +70,45 @@ class SpecialAllpages extends IncludableSpecialPage {
 	 * @param string $to dbKey we are ending listing at.
 	 */
 	function namespaceForm( $namespace = NS_MAIN, $from = '', $to = '' ) {
-	    global $wgScript;
-	    $t = $this->getTitle();
-
-	    $out  = Xml::openElement( 'div', array( 'class' => 'namespaceoptions' ) );
-	    $out .= Xml::openElement( 'form', array( 'method' => 'get', 'action' => $wgScript ) );
-	    $out .= Xml::hidden( 'title', $t->getPrefixedText() );
-	    $out .= Xml::openElement( 'fieldset' );
-	    $out .= Xml::element( 'legend', null, wfMsg( 'allpages' ) );
-	    $out .= Xml::openElement( 'table', array( 'id' => 'nsselect', 'class' => 'allpages' ) );
-	    $out .= "<tr>
-	            <td class='mw-label'>" .
-	                Xml::label( wfMsg( 'allpagesfrom' ), 'nsfrom' ) .
-	            "</td>
-	            <td class='mw-input'>" .
-	                Xml::input( 'from', 30, str_replace('_',' ',$from), array( 'id' => 'nsfrom' ) ) .
-	            "</td>
-	        </tr>
-	        <tr>
-	            <td class='mw-label'>" .
-	                Xml::label( wfMsg( 'allpagesto' ), 'nsto' ) .
-	            "</td>
-	            <td class='mw-input'>" .
-	                Xml::input( 'to', 30, str_replace('_',' ',$to), array( 'id' => 'nsto' ) ) .
-	            "</td>
-	        </tr>
-	        <tr>
-	            <td class='mw-label'>" .
-	                Xml::label( wfMsg( 'namespace' ), 'namespace' ) .
-	            "</td>
-	            <td class='mw-input'>" .
-	                Xml::namespaceSelector( $namespace, null ) . ' ' .
-	                Xml::submitButton( wfMsg( 'allpagessubmit' ) ) .
-	            "</td>
-	            </tr>";
-	    $out .= Xml::closeElement( 'table' );
-	    $out .= Xml::closeElement( 'fieldset' );
-	    $out .= Xml::closeElement( 'form' );
-	    $out .= Xml::closeElement( 'div' );
-	    return $out;
+		global $wgScript;
+		$t = $this->getTitle();
+	
+		$out  = Xml::openElement( 'div', array( 'class' => 'namespaceoptions' ) );
+		$out .= Xml::openElement( 'form', array( 'method' => 'get', 'action' => $wgScript ) );
+		$out .= Xml::hidden( 'title', $t->getPrefixedText() );
+		$out .= Xml::openElement( 'fieldset' );
+		$out .= Xml::element( 'legend', null, wfMsg( 'allpages' ) );
+		$out .= Xml::openElement( 'table', array( 'id' => 'nsselect', 'class' => 'allpages' ) );
+		$out .= "<tr>
+	<td class='mw-label'>" .
+			Xml::label( wfMsg( 'allpagesfrom' ), 'nsfrom' ) .
+			"	</td>
+	<td class='mw-input'>" .
+			Xml::input( 'from', 30, str_replace('_',' ',$from), array( 'id' => 'nsfrom' ) ) .
+			"	</td>
+</tr>
+<tr>
+	<td class='mw-label'>" .
+			Xml::label( wfMsg( 'allpagesto' ), 'nsto' ) .
+			"	</td>
+			<td class='mw-input'>" .
+			Xml::input( 'to', 30, str_replace('_',' ',$to), array( 'id' => 'nsto' ) ) .
+			"		</td>
+</tr>
+<tr>
+	<td class='mw-label'>" .
+			Xml::label( wfMsg( 'namespace' ), 'namespace' ) .
+			"	</td>
+			<td class='mw-input'>" .
+			Xml::namespaceSelector( $namespace, null ) . ' ' .
+			Xml::submitButton( wfMsg( 'allpagessubmit' ) ) .
+			"	</td>
+</tr>";
+		$out .= Xml::closeElement( 'table' );
+		$out .= Xml::closeElement( 'fieldset' );
+		$out .= Xml::closeElement( 'form' );
+		$out .= Xml::closeElement( 'div' );
+		return $out;
 	}
 
 	/**
@@ -307,7 +307,7 @@ class SpecialAllpages extends IncludableSpecialPage {
 					$t = Title::makeTitle( $s->page_namespace, $s->page_title );
 					if( $t ) {
 						$link = ( $s->page_is_redirect ? '<div class="allpagesredirect">' : '' ) .
-							$sk->makeKnownLinkObj( $t, htmlspecialchars( $t->getText() ), false, false ) .
+							$sk->linkKnown( $t, htmlspecialchars( $t->getText() ) ) .
 							($s->page_is_redirect ? '</div>' : '' );
 					} else {
 						$link = '[[' . htmlspecialchars( $s->page_title ) . ']]';
@@ -384,20 +384,34 @@ class SpecialAllpages extends IncludableSpecialPage {
 
 			# Do we put a previous link ?
 			if( isset( $prevTitle ) &&  $pt = $prevTitle->getText() ) {
-				$q = 'from=' . $prevTitle->getPartialUrl()
-					. ( $namespace ? '&namespace=' . $namespace : '' );
-				$prevLink = $sk->makeKnownLinkObj( $self,
-					wfMsgHTML( 'prevpage', htmlspecialchars( $pt ) ), $q );
+				$query = array( 'from' => $prevTitle->getPartialUrl() );
+
+				if( $namespace )
+					$query['namespace'] = $namespace;
+
+				$prevLink = $sk->linkKnown(
+					$self,
+					wfMsgHTML( 'prevpage', htmlspecialchars( $pt ) ),
+					array(),
+					$query
+				);
 				$out2 = $wgLang->pipeList( array( $out2, $prevLink ) );
 			}
 
 			if( $n == $this->maxPerPage && $s = $res->fetchObject() ) {
 				# $s is the first link of the next chunk
 				$t = Title::MakeTitle($namespace, $s->page_title);
-				$q = 'from=' . $t->getPartialUrl()
-					. ( $namespace ? '&namespace=' . $namespace : '' );
-				$nextLink = $sk->makeKnownLinkObj( $self,
-					wfMsgHtml( 'nextpage', htmlspecialchars( $t->getText() ) ), $q );
+				$query = array( 'from' => $t->getPartialUrl() );
+
+				if( $namespace )
+					$query['namespace'] = $namespace;
+
+				$nextLink = $sk->linkKnown(
+					$self,
+					wfMsgHtml( 'nextpage', htmlspecialchars( $t->getText() ) ),
+					array(),
+					$query
+				);
 				$out2 = $wgLang->pipeList( array( $out2, $nextLink ) );
 			}
 			$out2 .= "</td></tr></table>";

includes/specials/SpecialAncientpages.php
@@ -56,7 +56,10 @@ class AncientPagesPage extends QueryPage {
 
 		$d = $wgLang->timeanddate( wfTimestamp( TS_MW, $result->value ), true );
 		$title = Title::makeTitle( $result->namespace, $result->title );
-		$link = $skin->makeKnownLinkObj( $title, htmlspecialchars( $wgContLang->convert( $title->getPrefixedText() ) ) );
+		$link = $skin->linkKnown(
+			$title,
+			htmlspecialchars( $wgContLang->convert( $title->getPrefixedText() ) )
+		);
 		return wfSpecialList($link, htmlspecialchars($d) );
 	}
 }

includes/specials/SpecialBlockip.php
@@ -668,13 +668,21 @@ class IPBlockForm {
 	 */
 	private function getUnblockLink( $skin ) {
 		$list = SpecialPage::getTitleFor( 'Ipblocklist' );
+		$query = array( 'action' => 'unblock' );
+
 		if( $this->BlockAddress ) {
 			$addr = htmlspecialchars( strtr( $this->BlockAddress, '_', ' ' ) );
-			return $skin->makeKnownLinkObj( $list, wfMsgHtml( 'ipb-unblock-addr', $addr ),
-				'action=unblock&ip=' . urlencode( $this->BlockAddress ) );
+			$message = wfMsgHtml( 'ipb-unblock-addr', $addr );
+			$query['ip'] = $this->BlockAddress;
 		} else {
-			return $skin->makeKnownLinkObj( $list, wfMsgHtml( 'ipb-unblock' ),	'action=unblock' );
+			$message = wfMsgHtml( 'ipb-unblock' );
 		}
+		return $skin->linkKnown(
+			$list,
+			$message,
+			array(),
+			$query
+		);
 	}
 
 	/**
@@ -685,13 +693,22 @@ class IPBlockForm {
 	 */
 	private function getBlockListLink( $skin ) {
 		$list = SpecialPage::getTitleFor( 'Ipblocklist' );
+		$query = array();
+
 		if( $this->BlockAddress ) {
 			$addr = htmlspecialchars( strtr( $this->BlockAddress, '_', ' ' ) );
-			return $skin->makeKnownLinkObj( $list, wfMsgHtml( 'ipb-blocklist-addr', $addr ),
-				'ip=' . urlencode( $this->BlockAddress ) );
+			$message = wfMsgHtml( 'ipb-blocklist-addr', $addr );
+			$query['ip'] = $this->BlockAddress;
 		} else {
-			return $skin->makeKnownLinkObj( $list, wfMsgHtml( 'ipb-blocklist' ) );
+			$message = wfMsgHtml( 'ipb-blocklist' );
 		}
+
+		return $skin->linkKnown(
+			$list,
+			$message,
+			array(),
+			$query
+		);
 	}
 	
 	/**

includes/specials/SpecialBrokenRedirects.php
@@ -64,15 +64,30 @@ class BrokenRedirectsPage extends PageQueryPage {
 			return '<s>' . $skin->link( $fromObj ) . '</s>';
 		}
 
-		$from = $skin->makeKnownLinkObj( $fromObj ,'', 'redirect=no' );
-		$edit = $skin->makeKnownLinkObj( $fromObj, wfMsgHtml( 'brokenredirects-edit' ), 'action=edit' );
+		$from = $skin->linkKnown(
+			$fromObj,
+			null,
+			array(),
+			array( 'redirect' => 'no' )
+		);
+		$edit = $skin->linkKnown(
+			$fromObj,
+			wfMsgHtml( 'brokenredirects-edit' ),
+			array(),
+			array( 'action' => 'edit' )
+		);
 		$to   = $skin->makeBrokenLinkObj( $toObj );
 		$arr = $wgContLang->getArrow();
 
 		$out = "{$from} {$edit}";
 
 		if( $wgUser->isAllowed( 'delete' ) ) {
-			$delete = $skin->makeKnownLinkObj( $fromObj, wfMsgHtml( 'brokenredirects-delete' ), 'action=delete' );
+			$delete = $skin->linkKnown(
+				$fromObj,
+				wfMsgHtml( 'brokenredirects-delete' ),
+				array(),
+				array( 'action' => 'delete' )
+			);
 			$out .= " {$delete}";
 		}
 

includes/specials/SpecialConfirmemail.php
@@ -35,8 +35,12 @@ class EmailConfirmation extends UnlistedSpecialPage {
 			} else {
 				$title = SpecialPage::getTitleFor( 'Userlogin' );
 				$skin = $wgUser->getSkin();
-				$llink = $skin->makeKnownLinkObj( $title, wfMsgHtml( 'loginreqlink' ), 
-					'returnto=' . $this->getTitle()->getPrefixedUrl() );
+				$llink = $skin->linkKnown(
+					$title,
+					wfMsgHtml( 'loginreqlink' ),
+					array(),
+					array( 'returnto' => $this->getTitle()->getPrefixedUrl() )
+				);
 				$wgOut->addHTML( wfMsgWikiHtml( 'confirmemail_needlogin', $llink ) );
 			}
 		} else {

includes/specials/SpecialContributions.php
@@ -157,26 +157,41 @@ class SpecialContributions extends SpecialPage {
 			if( ( $id != 0 && $wgSysopUserBans ) || ( $id == 0 && IP::isIPAddress( $nt->getText() ) ) ) {
 				# Block link
 				if( $wgUser->isAllowed( 'block' ) )
-					$tools[] = $sk->makeKnownLinkObj( SpecialPage::getTitleFor( 'Blockip', 
-						$nt->getDBkey() ), wfMsgHtml( 'blocklink' ) );
+					$tools[] = $sk->linkKnown(
+						SpecialPage::getTitleFor( 'Blockip', $nt->getDBkey() ),
+						wfMsgHtml( 'blocklink' )
+					);
 				# Block log link
-				$tools[] = $sk->makeKnownLinkObj( SpecialPage::getTitleFor( 'Log' ), 
-					wfMsgHtml( 'sp-contributions-blocklog' ), 'type=block&page=' . $nt->getPrefixedUrl() );
+				$tools[] = $sk->linkKnown(
+					SpecialPage::getTitleFor( 'Log' ),
+					wfMsgHtml( 'sp-contributions-blocklog' ),
+					array(),
+					array(
+						'type' => 'block',
+						'page' => $nt->getPrefixedUrl()
+					)
+				);
 			}
 			# Other logs link
-			$tools[] = $sk->makeKnownLinkObj( SpecialPage::getTitleFor( 'Log' ), wfMsg( 'sp-contributions-logs' ), 
-				'user=' . $nt->getPartialUrl() );
+			$tools[] = $sk->linkKnown(
+				SpecialPage::getTitleFor( 'Log' ),
+				wfMsg( 'sp-contributions-logs' ),
+				array(),
+				array( 'user' => $nt->getPartialUrl() )
+			);
 
 			# Add link to deleted user contributions for priviledged users
 			if( $wgUser->isAllowed( 'deletedhistory' ) ) {
-				$tools[] = $sk->makeKnownLinkObj( SpecialPage::getTitleFor( 'DeletedContributions', 
-					$nt->getDBkey() ), wfMsgHtml( 'sp-contributions-deleted' ) );
+				$tools[] = $sk->linkKnown(
+					SpecialPage::getTitleFor( 'DeletedContributions', $nt->getDBkey() ),
+					wfMsgHtml( 'sp-contributions-deleted' )
+				);
 			}
 
 			# Add a link to change user rights for privileged users
 			$userrightsPage = new UserrightsPage();
 			if( 0 !== $id && $userrightsPage->userCanChangeRights( User::newFromId( $id ) ) ) {
-				$tools[] = $sk->makeKnownLinkObj(
+				$tools[] = $sk->linkKnown(
 					SpecialPage::getTitleFor( 'Userrights', $nt->getDBkey() ),
 					wfMsgHtml( 'sp-contributions-userrights' )
 				);
@@ -493,7 +508,12 @@ class ContribsPager extends ReverseChronologicalPager {
 		if( $row->rev_id == $row->page_latest ) {
 			$topmarktext .= '<span class="mw-uctop">' . $this->messages['uctop'] . '</span>';
 			if( !$row->page_is_new ) {
-				$difftext .= '(' . $sk->makeKnownLinkObj( $page, $this->messages['diff'], 'diff=0' ) . ')';
+				$difftext .= '(' . $sk->linkKnown(
+					$page,
+					$this->messages['diff'],
+					array(),
+					array( 'diff' => 0 )
+				) . ')';
 				# Add rollback link
 				if( $page->quickUserCan( 'rollback') && $page->quickUserCan( 'edit' ) ) {
 					$topmarktext .= ' '.$sk->generateRollback( $rev );
@@ -504,16 +524,33 @@ class ContribsPager extends ReverseChronologicalPager {
 		}
 		# Is there a visible previous revision?
 		if( $rev->userCan(Revision::DELETED_TEXT) ) {
-			$difftext = '(' . $sk->makeKnownLinkObj( $page, $this->messages['diff'],
-				'diff=prev&oldid='.$row->rev_id ) . ')';
+			$difftext = '(' . $sk->linkKnown(
+				$page,
+				$this->messages['diff'],
+				array(),
+				array(
+					'diff' => 'prev',
+					'oldid' => $row->rev_id
+				)
+			) . ')';
 		} else {
 			$difftext = '(' . $this->messages['diff'] . ')';
 		}
-		$histlink = '('.$sk->makeKnownLinkObj( $page, $this->messages['hist'], 'action=history' ) . ')';
+		$histlink = '('.$sk->linkKnown(
+			$page,
+			$this->messages['hist'],
+			array(),
+			array( 'action' => 'history' )
+		) . ')';
 
 		$comment = $wgContLang->getDirMark() . $sk->revComment( $rev, false, true );
 		$date = $wgLang->timeanddate( wfTimestamp( TS_MW, $row->rev_timestamp ), true );
-		$d = $sk->makeKnownLinkObj( $page, htmlspecialchars($date), 'oldid='.intval($row->rev_id) );
+		$d = $sk->linkKnown(
+			$page,
+			htmlspecialchars($date),
+			array(),
+			array( 'oldid' => intval( $row->rev_id ) )
+		);
 
 		if( $this->target == 'newbies' ) {
 			$userlink = ' . . ' . $sk->userLink( $row->rev_user, $row->rev_user_text );

commit 0514b9079713bc4493050ed59ec63ecca43663ba

* (bug 19442) Show/hide options on watchlist only work once * Regression is probably since r51572
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phase3@52772 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

RELEASE-NOTES
@@ -228,6 +228,7 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
    and attempted to upload with the source name. Now warns about not having an
    extension (since 0.ext is perfectly valid)
 * (bug 19468) Enotif preferences are now only displayed when they are turned on
+* (bug 19442) Show/hide options on watchlist only work once
 
 == API changes in 1.16 ==
 

includes/specials/SpecialWatchlist.php
@@ -252,108 +252,17 @@ function wfSpecialWatchlist( $par ) {
 
 	$cutofflinks = "\n" . wlCutoffLinks( $days, 'Watchlist', $nondefaults ) . "<br />\n";
 
-	# Spit out some control panel links
 	$thisTitle = SpecialPage::getTitleFor( 'Watchlist' );
-	$skin = $wgUser->getSkin();
 
-	$showLinktext = wfMsgHtml( 'show' );
-	$hideLinktext = wfMsgHtml( 'hide' );
-	# Hide/show minor edits
-	$label = $hideMinor ? $showLinktext : $hideLinktext;
-	$linkBits = array_merge(
-		array( 'hideMinor' => 1 - (int)$hideMinor ),
-		$nondefaults
-	);
-	$links[] = wfMsgHtml(
-		'rcshowhideminor',
-		$skin->linkKnown(
-			$thisTitle,
-			$label,
-			array(),
-			$linkBits
-		)
-	);
+	# Spit out some control panel links
+	$links[] = wlShowHideLink( $nondefaults, 'rcshowhideminor', 'hideMinor', $hideMinor );
+	$links[] = wlShowHideLink( $nondefaults, 'rcshowhidebots', 'hideBots', $hideBots );
+	$links[] = wlShowHideLink( $nondefaults, 'rcshowhideanons', 'hideAnons', $hideAnons );
+	$links[] = wlShowHideLink( $nondefaults, 'rcshowhideliu', 'hideLiu', $hideLiu );
+	$links[] = wlShowHideLink( $nondefaults, 'rcshowhidemine', 'hideOwn', $hideOwn );
 
-	# Hide/show bot edits
-	$label = $hideBots ? $showLinktext : $hideLinktext;
-	$linkBits = array_merge(
-		array( 'hideBots' => 1 - (int)$hideBots ),
-		$nondefaults
-	);
-	$links[] = wfMsgHtml(
-		'rcshowhidebots',
-		$skin->linkKnown(
-			$thisTitle,
-			$label,
-			array(),
-			$linkBits
-		)
-	);
-
-	# Hide/show anonymous edits
-	$label = $hideAnons ? $showLinktext : $hideLinktext;
-	$linkBits = array_merge(
-		array( 'hideAnons' => 1 - (int)$hideAnons ),
-		$nondefaults
-	);
-	$links[] = wfMsgHtml(
-		'rcshowhideanons',
-		$skin->linkKnown(
-			$thisTitle,
-			$label,
-			array(),
-			$linkBits
-		)
-	);
-
-	# Hide/show logged in edits
-	$label = $hideLiu ? $showLinktext : $hideLinktext;
-	$linkBits = array_merge(
-		array( 'hideLiu' => 1 - (int)$hideLiu ),
-		$nondefaults
-	);
-	$links[] = wfMsgHtml(
-		'rcshowhideliu',
-		$skin->linkKnown(
-			$thisTitle,
-			$label,
-			array(),
-			$linkBits
-		)
-	);
-
-	# Hide/show own edits
-	$label = $hideOwn ? $showLinktext : $hideLinktext;
-	$linkBits = array_merge(
-		array( 'hideOwn' => 1 - (int)$hideOwn ),
-		$nondefaults
-	);
-	$links[] = wfMsgHtml(
-		'rcshowhidemine',
-		$skin->linkKnown(
-			$thisTitle,
-			$label,
-			array(),
-			$linkBits
-		)
-	);
-
-	# Hide/show patrolled edits
 	if( $wgUser->useRCPatrol() ) {
-		$label = $hidePatrolled ? $showLinktext : $hideLinktext;
-		$linkBits = array_merge(
-			array( 'hidePatrolled' => 1 - (int)$hidePatrolled ),
-			$nondefaults
-		);
-		$links[] = wfMsgHtml(
-			'rcshowhidepatr',
-			$skin->linkKnown(
-				$thisTitle,
-				$label,
-				array(),
-				$linkBits
-			)
-		);
+		$links[] = wlShowHideLink( $nondefaults, 'rcshowhidepatr', 'hidePatrolled', $hidePatrolled );
 	}
 
 	# Namespace filter and put the whole form together.
@@ -438,6 +347,21 @@ function wfSpecialWatchlist( $par ) {
 	$dbr->freeResult( $res );
 	$wgOut->addHTML( $s );
 }
+
+function wlShowHideLink( $options, $message, $name, $value ) {
+	global $wgUser;
+
+	$showLinktext = wfMsgHtml( 'show' );
+	$hideLinktext = wfMsgHtml( 'hide' );
+	$title = SpecialPage::getTitleFor( 'Watchlist' );
+	$skin = $wgUser->getSkin();
+
+	$label = $value ? $showLinktext : $hideLinktext;
+	$options[$name] = 1 - (int) $value;
+
+	return wfMsgHtml( $message, $skin->linkKnown( $title, $label, array(), $options ) );
+}
+
 
 function wlHoursLink( $h, $page, $options = array() ) {
 	global $wgUser, $wgLang, $wgContLang;