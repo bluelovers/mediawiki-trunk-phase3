bluelovers/mediawiki-trunk-phase3

commit 23a9868fd694c2343473c526dd42e9c1c07bdb16

Reimplement r55685, r55686, r55687, with correct parsing of the associative array.  Also add message description to MessagesQqq as requested at r55685.
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phase3@55692 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Pager.php
@@ -879,10 +879,18 @@ abstract class TablePager extends IndexPager {
 	function getLimitSelect() {
 		global $wgLang;
 		$s = "<select name=\"limit\">";
-		foreach ( $this->mLimitsShown as $limit ) {
-			$selected = $limit == $this->mLimit ? 'selected="selected"' : '';
-			$formattedLimit = $wgLang->formatNum( $limit );
-			$s .= "<option value=\"$limit\" $selected>$formattedLimit</option>\n";
+		foreach ( $this->mLimitsShown as $key => $value ) {
+			# The pair is either $index => $limit, in which case the $value
+			# will be numeric, or $limit => $text, in which case the $value
+			# will be a string.
+			if( is_int( $value ) ){
+				$limit = $text = $value;
+			} else {
+				$limit = $key;
+				$text = $value;
+			}
+			$selected = ( $limit == $this->mLimit ? 'selected="selected"' : '' );
+			$s .= "<option value=\"$limit\" $selected>$text</option>\n";
 		}
 		$s .= "</select>";
 		return $s;

RELEASE-NOTES
@@ -197,6 +197,8 @@ this. Was used when mwEmbed was going to be an extension.
 * (bug 20404) Custom fields in the user creation form template can now have
   detail labels in prefsectiontip divs.
 * MakeSysop and MakeBot are now aliases for Special:UserRights
+* IndexPager->mLimitsShown can now be an associative array of limit => text-to-
+  display-in-limit-form.
 
 === Bug fixes in 1.16 ===
 

includes/specials/SpecialAllmessages.php
@@ -128,6 +128,7 @@ class AllmessagesTablePager extends TablePager {
 
 	var $messages  = null;
 	var $talkPages = null;
+	public $mLimitsShown;
 
 	function __construct( $page, $conds, $langObj = null ) {
 		parent::__construct();
@@ -135,6 +136,10 @@ class AllmessagesTablePager extends TablePager {
 		$this->mPage = $page;
 		$this->mConds = $conds;
 		$this->mDefaultDirection = true; // always sort ascending
+		// We want to have an option for people to view *all* the messages, 
+		// so they can use Ctrl+F to search them.  5000 is the maximum that 
+		// will get through WebRequest::getLimitOffset().
+		$this->mLimitsShown = array( 20, 50, 100, 250, 500, 5000 => wfMsg('limitall') );
 
 		global $wgLang, $wgContLang, $wgRequest;
 

languages/messages/MessagesEn.php
@@ -3814,6 +3814,7 @@ Others will be hidden by default.
 'watchlistall2'    => 'all',
 'namespacesall'    => 'all',
 'monthsall'        => 'all',
+'limitall'         => 'all',
 
 # E-mail address confirmation
 'confirmemail'             => 'Confirm e-mail address',

languages/messages/MessagesLzh.php
@@ -2433,6 +2433,7 @@ $1',
 'watchlistall2'    => '全',
 'namespacesall'    => '全',
 'monthsall'        => '全',
+'limitall'         => '全',
 
 # E-mail address confirmation
 'confirmemail'             => '核郵驛',

languages/messages/MessagesQqq.php
@@ -2997,6 +2997,9 @@ Please leave the link http://www.mediawiki.org/wiki/Manual:External_editors exac
 'monthsall'        => 'Used in a drop-down box on [[Special:Contributions]] as an option for "all months". See also [[MediaWiki:Month/{{SUBPAGENAME}}]].
 
 {{Identical|All}}',
+'limitall'      => 'Used on [[Special:AllMessages]] (and potentially other TablePager based tables) to display "all" the messages. 
+
+{{Identical|All}}',
 
 # E-mail address confirmation
 'confirmemail_needlogin'   => 'Used on [[Special:ConfirmEmail]] when you are logged out.

languages/messages/MessagesYue.php
@@ -2956,6 +2956,7 @@ Variants for Chinese language
 'watchlistall2'    => '全部',
 'namespacesall'    => '全部',
 'monthsall'        => '全部',
+'limitall'         => '全部',
 
 # E-mail address confirmation
 'confirmemail'             => '確認電郵地址',

languages/messages/MessagesZh_hans.php
@@ -2993,6 +2993,7 @@ Variants for Chinese language
 'watchlistall2'    => '全部',
 'namespacesall'    => '全部',
 'monthsall'        => '全部',
+'limitall'         => '全部',
 
 # E-mail address confirmation
 'confirmemail'             => '确认邮箱地址',

commit 0ed4a136a28e7d25b6a8b4dc7ce2ff5a67d6b164

Add number formatting for numeric limits, regression in r55692
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phase3@56077 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Pager.php
@@ -884,7 +884,8 @@ abstract class TablePager extends IndexPager {
 			# will be numeric, or $limit => $text, in which case the $value
 			# will be a string.
 			if( is_int( $value ) ){
-				$limit = $text = $value;
+				$limit = $value;
+				$text = $wgLang->formatNum( $limit );
 			} else {
 				$limit = $key;
 				$text = $value;