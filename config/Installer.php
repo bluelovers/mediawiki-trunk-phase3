bluelovers/mediawiki-trunk-phase3

commit 027bb213795c5827e5a2218481db5f2ed8cd23dc

DatabaseMssql class and related changes
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phase3@70608 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

config/Installer.php
@@ -87,6 +87,15 @@ $ourdb['sqlite'] = array(
 	'serverless' =>  true
 );
 
+$ourdb['mssql'] = array(
+	'fullname'   => 'Microsoft SQL Server',
+	'havedriver' => 0,
+	'compile'    => 'sqlsrv',
+	'bgcolor'    => '#cccccc',
+	'rootuser'   => 'root',
+	'serverless' => false
+);
+
 $ourdb['ibm_db2'] = array(
 	'fullname'   => 'DB2',
 	'havedriver' => 0,
@@ -910,6 +919,44 @@ if( $conf->posted && ( 0 == count( $errs ) ) ) {
 
 			if( !$ok ) { continue; }
 		}
+		else if ( $conf->DBtype == 'mssql' ) {
+			error_reporting( E_ALL );
+			$wgSuperUser = '';
+			# # Possible connect as a superuser
+			if ( $useRoot ) {
+				$wgDBsuperuser = $conf->RootUser;
+				echo( "<li>Attempting to connect to database \"{$conf->DBtype}\" as superuser \"{$wgDBsuperuser}\"" );
+				$wgDatabase = $dbc->newFromParams(
+										$conf->DBserver,
+										$conf->RootUser,
+										$conf->RootPW,
+										false,
+										false,
+										1
+									);
+				if ( !$wgDatabase->isOpen() ) {
+					echo( " error: {$wgDatabase->lastError()}</li>\n" );
+					$errs['DBserver'] = 'Could not connect to database as superuser';
+					$errs['RootUser'] = 'Check username';
+					$errs['RootPW'] = 'and password';
+					continue;
+				}
+				$wgDatabase->initial_setup( $conf->RootPW, $conf->DBtype );
+			}
+			echo( "<li>Attempting to connect to database \"{$wgDBname}\" as \"{$wgDBuser}\"..." );
+			$wgDatabase = $dbc->newFromParams(
+									$conf->DBserver,
+									$conf->DBuser,
+									$conf->DBpassword,
+									$conf->DBname,
+									1
+								);
+			if ( !$wgDatabase->isOpen() ) {
+				echo( " error: {$wgDatabase->lastError()} </li>\n" );
+			} else {
+				$myver = $wgDatabase->getServerVersion();
+			}
+		}
 		else if( $conf->DBtype == 'ibm_db2' ) {
 			if( $useRoot ) {
 				$db_user = $conf->RootUser;
@@ -1659,6 +1706,13 @@ if( count( $errs ) ) {
 	</div>
 	</fieldset>
 
+	
+	<?php database_switcher( 'mssql' ); ?>
+	<div class="config-desc">
+		<p>No MS SQL Server specific options at this time.</p>
+	</div>
+	
+	
 	<?php database_switcher('ibm_db2'); ?>
 	<div class="config-input"><?php
 		aField( $conf, "DBport_db2", "Database port:" );

includes/AutoLoader.php
@@ -343,6 +343,7 @@ $wgAutoloadLocalClasses = array(
 	'ChronologyProtector' => 'includes/db/LBFactory.php',
 	'Database' => 'includes/db/DatabaseMysql.php',
 	'DatabaseBase' => 'includes/db/Database.php',
+	'DatabaseMssql' => 'includes/db/DatabaseMssql.php',
 	'DatabaseMysql' => 'includes/db/DatabaseMysql.php',
 	'DatabaseOracle' => 'includes/db/DatabaseOracle.php',
 	'DatabasePostgres' => 'includes/db/DatabasePostgres.php',
@@ -511,6 +512,7 @@ $wgAutoloadLocalClasses = array(
 	'SearchEngine' => 'includes/search/SearchEngine.php',
 	'SearchHighlighter' => 'includes/search/SearchEngine.php',
 	'SearchIBM_DB2' => 'includes/search/SearchIBM_DB2.php',
+	'SearchMssql' => 'includes/search/SearchMssql.php',
 	'SearchMySQL4' => 'includes/search/SearchMySQL4.php',
 	'SearchMySQL' => 'includes/search/SearchMySQL.php',
 	'SearchOracle' => 'includes/search/SearchOracle.php',

includes/Block.php
@@ -857,6 +857,16 @@ class Block {
 	public static function infinity() {
 		# This is a special keyword for timestamps in PostgreSQL, and
 		# works with CHAR(14) as well because "i" sorts after all numbers.
+
+			# BEGIN DatabaseMssql hack
+			# Since MSSQL doesn't recognize the infinity keyword, set date manually.
+			# TO-DO: Refactor for better DB portability and remove magic date
+			$dbw = wfGetDB( DB_MASTER );
+			if ( $dbw instanceof DatabaseMssql ) {
+				return '3000-01-31 00:00:00.000';
+			}
+			# End DatabaseMssql hack
+
 		return 'infinity';
 	}
 

includes/db/Database.php
@@ -1878,11 +1878,11 @@ abstract class DatabaseBase {
 		} while( $this->wasDeadlock() && --$tries > 0 );
 		$this->ignoreErrors( $oldIgnore );
 		if ( $tries <= 0 ) {
-			$this->query( 'ROLLBACK', $myFname );
+			$this->rollback( $myFname );
 			$this->reportQueryError( $error, $errno, $sql, $fname );
 			return false;
 		} else {
-			$this->query( 'COMMIT', $myFname );
+			$this->commit( $myFname );
 			return $retVal;
 		}
 	}

includes/search/SearchMssql.php
@@ -1,0 +1,240 @@
+<?php
+# Copyright (C) 2004 Brion Vibber <brion@pobox.com>
+# http://www.mediawiki.org/
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# http://www.gnu.org/copyleft/gpl.html
+
+/**
+ * Search engine hook base class for Mssql (ConText).
+ * @addtogroup Search
+ */
+class SearchMssql extends SearchEngine {
+
+	function __construct( $db ) {
+		$this->db = $db;
+	}
+
+	/**
+	 * Perform a full text search query and return a result set.
+	 *
+	 * @param string $term - Raw search term
+	 * @return MssqlSearchResultSet
+	 * @access public
+	 */
+	function searchText( $term ) {
+		$resultSet = $this->db->resultObject( $this->db->query( $this->getQuery( $this->filter( $term ), true ) ) );
+		return new MssqlSearchResultSet( $resultSet, $this->searchTerms );
+	}
+
+	/**
+	 * Perform a title-only search query and return a result set.
+	 *
+	 * @param string $term - Raw search term
+	 * @return MssqlSearchResultSet
+	 * @access public
+	 */
+	function searchTitle( $term ) {
+		$resultSet = $this->db->resultObject( $this->db->query( $this->getQuery( $this->filter( $term ), false ) ) );
+		return new MssqlSearchResultSet( $resultSet, $this->searchTerms );
+	}
+
+
+	/**
+	 * Return a partial WHERE clause to exclude redirects, if so set
+	 * @return string
+	 * @private
+	 */
+	function queryRedirect() {
+		if ( $this->showRedirects ) {
+			return '';
+		} else {
+			return 'AND page_is_redirect=0';
+		}
+	}
+
+	/**
+	 * Return a partial WHERE clause to limit the search to the given namespaces
+	 * @return string
+	 * @private                           
+	 */
+	function queryNamespaces() {
+		$namespaces = implode( ',', $this->namespaces );
+		if ( $namespaces == '' ) {
+			$namespaces = '0';
+		}
+		return 'AND page_namespace IN (' . $namespaces . ')';
+	}
+
+	/**
+	 * Return a LIMIT clause to limit results on the query.
+	 * @return string
+	 * @private
+	 */
+	function queryLimit( $sql ) {
+		return $this->db->limitResult( $sql, $this->limit, $this->offset );
+	}
+
+	/**
+	 * Does not do anything for generic search engine
+	 * subclasses may define this though
+	 * @return string
+	 * @private
+	 */
+	function queryRanking( $filteredTerm, $fulltext ) {
+		return ' ORDER BY ftindex.[RANK] DESC'; // return ' ORDER BY score(1)';
+	}
+
+	/**
+	 * Construct the full SQL query to do the search.
+	 * The guts shoulds be constructed in queryMain()
+	 * @param string $filteredTerm
+	 * @param bool $fulltext
+	 * @private
+	 */
+	function getQuery( $filteredTerm, $fulltext ) {
+		return $this->queryLimit( $this->queryMain( $filteredTerm, $fulltext ) . ' ' .
+			$this->queryRedirect() . ' ' .
+			$this->queryNamespaces() . ' ' .
+			$this->queryRanking( $filteredTerm, $fulltext ) . ' ' );
+	}
+
+
+	/**
+	 * Picks which field to index on, depending on what type of query.
+	 * @param bool $fulltext
+	 * @return string
+	 */
+	function getIndexField( $fulltext ) {
+		return $fulltext ? 'si_text' : 'si_title';
+	}
+
+	/**
+	 * Get the base part of the search query.
+	 *
+	 * @param string $filteredTerm
+	 * @param bool $fulltext
+	 * @return string
+	 * @private
+	 */
+	function queryMain( $filteredTerm, $fulltext ) {
+		$match = $this->parseQuery( $filteredTerm, $fulltext );
+		$page        = $this->db->tableName( 'page' );
+		$searchindex = $this->db->tableName( 'searchindex' );
+		
+		return 'SELECT page_id, page_namespace, page_title, ftindex.[RANK]' .
+			"FROM $page,FREETEXTTABLE($searchindex , $match, LANGUAGE 'English') as ftindex " .
+			'WHERE page_id=ftindex.[KEY] ';
+	}
+
+	/** @todo document */
+	function parseQuery( $filteredText, $fulltext ) {
+		global $wgContLang;
+		$lc = SearchEngine::legalSearchChars();
+		$this->searchTerms = array();
+
+		# FIXME: This doesn't handle parenthetical expressions.
+		$m = array();
+		$q = array();
+
+		if ( preg_match_all( '/([-+<>~]?)(([' . $lc . ']+)(\*?)|"[^"]*")/',
+			$filteredText, $m, PREG_SET_ORDER ) ) {
+			foreach ( $m as $terms ) {
+				$q[] = $terms[1] . $wgContLang->normalizeForSearch( $terms[2] );
+
+				if ( !empty( $terms[3] ) ) {
+					$regexp = preg_quote( $terms[3], '/' );
+					if ( $terms[4] )
+						$regexp .= "[0-9A-Za-z_]+";
+				} else {
+					$regexp = preg_quote( str_replace( '"', '', $terms[2] ), '/' );
+				}
+				$this->searchTerms[] = $regexp;
+			}
+		}
+
+		$searchon = $this->db->strencode( join( ',', $q ) );
+		$field = $this->getIndexField( $fulltext );
+		return "$field, '$searchon'";
+	}
+
+	/**
+	 * Create or update the search index record for the given page.
+	 * Title and text should be pre-processed.
+	 *
+	 * @param int $id
+	 * @param string $title
+	 * @param string $text
+	 */
+	function update( $id, $title, $text ) {
+		// We store the column data as UTF-8 byte order marked binary stream
+		// because we are invoking the plain text IFilter on it so that, and we want it 
+		// to properly decode the stream as UTF-8.  SQL doesn't support UTF8 as a data type
+		// but the indexer will correctly handle it by this method.  Since all we are doing
+		// is passing this data to the indexer and never retrieving it via PHP, this will save space
+		$table = $this->db->tableName( 'searchindex' );
+		$utf8bom = '0xEFBBBF';
+		$si_title = $utf8bom . bin2hex( $title );
+		$si_text = $utf8bom . bin2hex( $text );
+		$sql = "DELETE FROM $table WHERE si_page = $id;";
+		$sql .= "INSERT INTO $table (si_page, si_title, si_text) VALUES ($id, $si_title, $si_text)";
+		return $this->db->query( $sql, 'SearchMssql::update' );
+	}
+
+	/**
+	 * Update a search index record's title only.
+	 * Title should be pre-processed.
+	 *
+	 * @param int $id
+	 * @param string $title
+	 */
+	function updateTitle( $id, $title ) {
+		$table = $this->db->tableName( 'searchindex' );
+
+		// see update for why we are using the utf8bom
+		$utf8bom = '0xEFBBBF';
+		$si_title = $utf8bom . bin2hex( $title );
+		$sql = "DELETE FROM $table WHERE si_page = $id;";
+		$sql .= "INSERT INTO $table (si_page, si_title, si_text) VALUES ($id, $si_title, 0x00)";
+		return $this->db->query( $sql, 'SearchMssql::updateTitle' );
+	}
+}
+
+/**
+ * @addtogroup Search
+ */
+class MssqlSearchResultSet extends SearchResultSet {
+	function __construct( $resultSet, $terms ) {
+		$this->mResultSet = $resultSet;
+		$this->mTerms = $terms;
+	}
+
+	function termMatches() {
+		return $this->mTerms;
+	}
+
+	function numRows() {
+		return $this->mResultSet->numRows();
+	}
+
+	function next() {
+		$row = $this->mResultSet->fetchObject();
+		if ( $row === false )
+			return false;
+		return new SearchResult( $row );
+	}
+}
+
+

includes/specials/SpecialAncientpages.php
@@ -54,6 +54,9 @@ class AncientPagesPage extends QueryPage {
 			case 'sqlite':
 				$epoch = 'rev_timestamp';
 				break;
+			case 'mssql':
+				$epoch = 'DATEDIFF(s,CONVERT(datetime,\'1/1/1970\'),rev_timestamp)';
+				break;
 			default:
 				$epoch = 'EXTRACT(epoch FROM rev_timestamp)';
 		}

includes/specials/SpecialSearch.php
@@ -119,7 +119,41 @@ class SpecialSearch {
 		wfProfileIn( __METHOD__ );
 
 		$sk = $wgUser->getSkin();
-
+		
+		$beginSearchForm = Xml::openElement(
+			'form',
+			array(
+				'id' => ( $this->searchAdvanced ? 'powersearch' : 'search' ),
+				'method' => 'get',
+				'action' => $wgScript
+			)
+		);
+		$mwSearchTopTable =
+			Xml::openElement( 'table', array( 'id' => 'mw-search-top-table', 'border' => 0, 'cellpadding' => 0, 'cellspacing' => 0 ) ) .
+			Xml::openElement( 'tr' ) .
+			Xml::openElement( 'td' ) . "\n"	.
+			$this->shortDialog( $term ) .
+			Xml::closeElement( 'td' ) .
+			Xml::closeElement( 'tr' ) .
+			Xml::closeElement( 'table' );
+		
+		// moved to check for empty or null search string before running query
+		// to prevent NULL fulltext search error in SQL Server
+		$filePrefix = $wgContLang->getFormattedNsText( NS_FILE ) . ':';
+		if ( trim( $term ) === '' || $filePrefix === trim( $term ) ) {
+			$wgOut->addHTML( $beginSearchForm );
+			$wgOut->addHTML( $mwSearchTopTable );
+			$wgOut->addHTML( $this->searchFocus() );
+			$wgOut->addHTML( $this->formHeader( $term, 0, 0 ) );
+			if ( $this->searchAdvanced ) {
+				$wgOut->addHTML( $this->powerSearchBox( $term ) );
+			}
+			$wgOut->addHTML( Xml::closeElement( 'form' ) );
+			// Empty query -- straight view of search form
+			wfProfileOut( __METHOD__ );
+			return;
+		}
+		
 		$this->searchEngine = SearchEngine::create();
 		$search =& $this->searchEngine;
 		$search->setLimitOffset( $this->limit, $this->offset );
@@ -194,41 +228,15 @@ class SpecialSearch {
 		}
 		// start rendering the page
 		$wgOut->addHtml(
-			Xml::openElement(
-				'form',
-				array(
-					'id' => ( $this->searchAdvanced ? 'powersearch' : 'search' ),
-					'method' => 'get',
-					'action' => $wgScript
-				)
-			)
+			$beginSearchForm
 		);
 		$wgOut->addHtml(
-			Xml::openElement( 'table', array( 'id'=>'mw-search-top-table', 'border'=>0, 'cellpadding'=>0, 'cellspacing'=>0 ) ) .
-			Xml::openElement( 'tr' ) .
-			Xml::openElement( 'td' ) . "\n"	.
-			$this->shortDialog( $term ) .
-			Xml::closeElement('td') .
-			Xml::closeElement('tr') .
-			Xml::closeElement('table')
+			$mwSearchTopTable
 		);
 
 		// Sometimes the search engine knows there are too many hits
 		if( $titleMatches instanceof SearchResultTooMany ) {
 			$wgOut->addWikiText( '==' . wfMsg( 'toomanymatches' ) . "==\n" );
-			wfProfileOut( __METHOD__ );
-			return;
-		}
-
-		$filePrefix = $wgContLang->getFormattedNsText(NS_FILE).':';
-		if( trim( $term ) === '' || $filePrefix === trim( $term ) ) {
-			$wgOut->addHTML( $this->searchFocus() );
-			$wgOut->addHTML( $this->formHeader($term, 0, 0));
-			if( $this->searchAdvanced ) {
-				$wgOut->addHTML( $this->powerSearchBox( $term ) );
-			} 
-			$wgOut->addHTML( '</form>' );
-			// Empty query -- straight view of search form
 			wfProfileOut( __METHOD__ );
 			return;
 		}

commit 0fea341fb3834c741542072c7506d43082df5343

Fixed yet another breakage introduced by r70608: bug 24764 - broken old installer form. Next time please use a HTML validator every time you change HTML output.
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phase3@71386 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

config/Installer.php
@@ -1707,6 +1707,7 @@ if( count( $errs ) ) {
 	<div class="config-desc">
 		<p>No MS SQL Server specific options at this time.</p>
 	</div>
+	</fieldset>
 	
 	
 	<?php database_switcher('ibm_db2'); ?>